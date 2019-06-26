cc4b7e38a5d1954f73d352da95b410ebac501a69
Omar Sanchez <osanchez@umass.edu>
2019-06-26 11:35:24 -0400

    notes

--- src/FirstNeuralNetwork.py
+++ src/FirstNeuralNetwork.py
@@ -9,11 +9,13 @@ def sigmoid_derivative(x):
     return x * (1 - x)
 
 
+# input data that the model will train on
 training_inputs = np.array([[0, 0, 1],
                             [1, 1, 1],
                             [1, 0, 1],
                             [0, 1, 1]])
 
+# expected results for input data
 training_output = np.array([[0, 1, 1, 0]]).T
 
 np.random.seed(1)
@@ -23,8 +25,8 @@ synaptic_weights = 2 * np.random.random((3, 1)) - 1
 print("Random starting synaptic weights: ")
 print(synaptic_weights)
 
+# train the model for x iterations, the more iterations the more accurate result
 for iteration in range(20000):
-
     input_layer = training_inputs
     outputs = sigmoid(np.dot(input_layer, synaptic_weights))
 
@@ -39,4 +41,3 @@ print(synaptic_weights)
 
 print("Outputs after training: ")
 print(outputs)
-
