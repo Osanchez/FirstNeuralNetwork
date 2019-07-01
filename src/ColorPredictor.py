62f1a7d345c0e60476e027a048f849f7cd1781ca
Omar Sanchez <osanchez@umass.edu>
2019-06-30 22:33:07 -0400

    color predictor trains, but might need better training data

--- src/ColorPredictor.py
+++ src/ColorPredictor.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 class ColorPredictor:
 
     def __init__(self):
-        np.random.seed(1337)
+        np.random.seed(100)
         self.synaptic_weights = 2 * np.random.random((3, 1)) - 1
         print(self.synaptic_weights)
 
@@ -15,14 +15,25 @@ class ColorPredictor:
     def sigmoid_derivative(self, x):
         return x * (1 - x)
 
+    def predict(self, inputs):
+        inputs = inputs.astype(float)
+        output = self.sigmoid(np.dot(inputs, self.synaptic_weights))
+        result = (output > 0.5).astype(int)
+
+        if result == 1:
+            return "Black"
+        else:
+            return "White"
+
     def think(self, inputs):
         inputs = inputs.astype(float)
+        inputs = np.true_divide(inputs, 255)
         output = self.sigmoid(np.dot(inputs, self.synaptic_weights))
         return output
 
     def train(self, training_inputs, training_outputs, training_iterations):
 
-            for iterations in range(training_iterations):
+            for epoch in range(training_iterations):
                 output = self.think(training_inputs)
                 error = training_outputs - output
                 adjustments = np.dot(training_inputs.T, error * self.sigmoid_derivative(output))
@@ -33,26 +44,50 @@ if __name__ == "__main__":
     print("Initial Synaptic Weights:")
     predictor = ColorPredictor()
 
+    # training data
+    randomized_colors = np.array([
+        [[0, 0, 0]],
+        [[255, 255, 255]],
+    ])
+
     # input data that the model will train on
-    randomized_colors = np.array(np.random.randint(0, 255, size=(4, 1, 3)))
-    print("Randomized Colors: ")
+    print("Training Data: ")
+    training_inputs = np.array(list(x[0] for x in randomized_colors))
+    print(training_inputs)
 
-    for color in randomized_colors:
-        print(color[0])
-
-    # displays the random colors
+    # displays the random colors in sci view
     plt.imshow(randomized_colors)
     plt.show()
 
     # expected results for input data
-    # 0 - White, 1 - Black
-    training_output = np.array([[1, 0, 1, 0]]).T
-
-    print("Expected Values: ")
+    # 1 - Black, 0 - White
+    training_output = np.array([[0, 1]]).T
     print(training_output)
 
-    # TODO: Training
-    # predictor.train(training_inputs, training_output, 10000)
+    predictor.train(training_inputs, training_output, 100000)
 
     print("Synaptic weights after training: ")
     print(predictor.synaptic_weights)
+
+    print()
+
+    keep_predicting = True
+
+    while keep_predicting:
+        A = str(input("Input 1 (R): "))
+        B = str(input("Input 2 (G): "))
+        C = str(input("Input 3 (B): "))
+
+        print("New situation: input data = ", A, B, C)
+
+        print("Predict Output: ")
+        user_input = np.array([A, B, C])
+
+        print(predictor.predict(user_input))
+
+        continue_pred = input("Keep predicting? (y/n): ").lower()
+
+        if continue_pred == "n":
+            keep_predicting = False
+
+
